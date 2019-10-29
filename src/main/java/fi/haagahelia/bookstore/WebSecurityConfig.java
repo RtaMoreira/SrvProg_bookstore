a8015b2eed161b8223aa717904b360529ea8655b
RtaMoreira <rita.moreira@students.hevs.ch>
2019-10-29 12:35:08 +0200

    debug

--- src/main/java/fi/haagahelia/bookstore/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/bookstore/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
       .formLogin()
       		.loginPage("/login")
-      		.defaultSuccessUrl("/booklist")
+      		.defaultSuccessUrl("/booklist", true)
       		.permitAll()
       		.and()
       .logout()
